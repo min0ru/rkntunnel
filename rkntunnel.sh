d7548b39e5c87bfb911a5d27074b376d8cc61956
suravtsov <suravtsov@vistgroup.ru>
2019-06-28 21:00:05 +0300

    Migrated to rublacklist.net api

--- rkntunnel.sh
+++ rkntunnel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh rkntunnel -D 1080 -fN
+ssh rkntunnel-tor -D 1080 -fN
 
 echo "Initializing iptables"
 sudo iptables -t nat -N REDSOCKS
@@ -12,9 +12,11 @@ sudo ipset create rkn hash:net maxelem 20000000
 sudo ipset flush rkn
 
 echo "Downloading banned ip list"
-for ip in $(python3 antizapret_csv.py)
+#for ip in $(python3 antizapret_csv.py)
+for ip in $(wget https://reestr.rublacklist.net/api/v2/ips/csv -q -O - | sed '/:/d' - | tr "," " ")
 do
-    echo "Adding ip $ip"
+    #echo "Adding ip $ip"
+    >&2 echo -n ". "
 	sudo ipset -A rkn ${ip}
 done
 
