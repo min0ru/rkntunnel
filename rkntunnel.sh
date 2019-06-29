e4639c394f682696f4b3d256119e407970804e19
min0ru <2010min0ru@gmail.com>
2019-06-29 16:27:06 +0300

    Cleaned some old stuff

--- rkntunnel.sh
+++ rkntunnel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh rkntunnel-tor -D 1080 -fN
+autossh rkntunnel-tor -D 1080 -fN
 
 echo "Initializing iptables"
 sudo iptables -t nat -N REDSOCKS
@@ -12,10 +12,8 @@ sudo ipset create rkn hash:net maxelem 20000000
 sudo ipset flush rkn
 
 echo "Downloading banned ip list"
-#for ip in $(python3 antizapret_csv.py)
 for ip in $(wget https://reestr.rublacklist.net/api/v2/ips/csv -q -O - | sed '/:/d' - | tr "," " ")
 do
-    #echo "Adding ip $ip"
     >&2 echo -n ". "
 	sudo ipset -A rkn ${ip}
 done
